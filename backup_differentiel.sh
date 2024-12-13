63d29ac79936694ce8be9bb8824fd2f6f8cc4cef
Paolo Masse <paolocisse6@gmail.com>
2024-12-13 14:46:58 +0100

    ajout explication du retrait de --delete

--- backup_differentiel.sh
+++ backup_differentiel.sh
@@ -40,7 +40,7 @@ mkdir -p "$BACKUP_DIR"
 #je commence la sauvegarde
 
 
-echo "Début de la sauvegarde automatique a 02h00: $(date)" >> "$LOG_FILE"
+echo "Début de la sauvegarde automatique: $(date)" >> "$LOG_FILE"
 
 echo "Début de la sauvegarde automatique à 02h00 programmé avec cron"
 #synchronisation avec hard links pour les fichiers inchangés
